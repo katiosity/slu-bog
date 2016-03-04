0f68b6ad841424159ed1605f10f0317dddee4e2a
katiosity <lovan.kathy@gmail.com>
2016-03-03 16:08:25 -0800

    added create, new, edit, show methods to creatures controller; updated associated views

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -4,20 +4,31 @@ class CreaturesController < ApplicationController
   end
 
   def create
+    Creature.create creature_params
+    redirect_to creatures_path
   end
 
   def new
+    @creature = Creature.new
   end
 
   def edit
+    @creature = Creature.find params[:id]
   end
 
   def show
+    @creature = Creature.find params[:id]
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:content, :name)
   end
 end
